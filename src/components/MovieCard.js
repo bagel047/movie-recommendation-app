e62e94dedb3cc2d9e3826e70a781545b633b9d63
bagel047e <bage.josifoska@gmail.com>
2024-08-21 14:59:00 +0200

    add MovieCard.js remove TVSlider.js

---
+++ src/components/MovieCard.js
@@ -0,0 +1,112 @@
+import { useEffect, useState } from "react";
+import Bookmark from "./Bookmark";
+import StarRating from "./StarRating";
+import { options } from "../shared";
+
+export default function MovieCard(props) {
+    const [details, setDetails] = useState();
+    const [name, setName] = useState("");
+    const [releaseDate, setReleaseDate] = useState("");
+    const [runtime, setRuntime] = useState("N/A");
+    const [seasons, setSeasons] = useState("N/A");
+
+    useEffect(() => {
+        fetch(
+            `https://api.themoviedb.org/3/${props.type}/${props.data.id.toString()}?language=en-US`,
+            options
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                setDetails(data);
+                props.setFetchedDetails((prev) => new Set(prev).add(props.data.id));
+            });
+    }, []);
+
+    useEffect(() => {
+        if (props.type === "movie") {
+            // movie
+            setName(details?.title);
+            setReleaseDate(details?.release_date);
+            setRuntime(details?.runtime);
+        } else {
+            // tv show
+            setName(details?.name);
+            setReleaseDate(details?.first_air_date);
+            setSeasons(details?.number_of_seasons);
+        }
+    }, [details]);
+
+    function getRatingColor(rating) {
+        if (rating >= 8) {
+            return "bg-green-400 text-white dark:bg-green-500 dark:text-white";
+        } else if (rating >= 7) {
+            return "bg-lime-400 text-white dark:bg-lime-400 dark:text-white";
+        } else if (rating >= 6) {
+            return "bg-yellow-400 text-white dark:bg-yellow-500 dark:text-white";
+        } else if (rating >= 5) {
+            return "bg-orange-400 text-white dark:bg-orange-500 dark:text-white";
+        } else if (rating >= 1) {
+            return "bg-red-400 text-white dark:bg-red-500 dark:text-white";
+        } else {
+            return "bg-gray-200 text-white dark:bg-gray-300 dark:text-white";
+        }
+    }
+
+    return (
+        <div className="mr-3 w-56 h-fit bg-inherit inline-block snap-start hover:scale-[1.06] ease-in-out duration-150">
+            <div className="relative w-full overflow-hidden">
+                <a href="#">
+                    <img
+                        id="moviePoster"
+                        className="rounded-t-sm aspect-video object-cover w-full h-full"
+                        src={"https://image.tmdb.org/t/p/w500" + props.data?.poster_path}
+                        alt={name + " Poster"}
+                    />
+                </a>
+            </div>
+            <div className="w-full">
+                <a href="#">
+                    <h5
+                        className="pt-3 text-md text-white dark:text-white font-semibold truncate w-full overflow-hidden"
+                        title={name}
+                    >
+                        {name}
+                    </h5>
+                </a>
+                <span className="mr-2 text-gray-900 dark:text-slate-300 text-xs">
+                    {details.genres[0] ? details.genres[0].name : null}
+                    {details.genres[1] ? ", " + details.genres[1].name : null}
+                </span>
+                <div className="flex items-center mt-2.5 mb-5">
+                    <StarRating rating={details?.vote_average || 0} />
+
+                    <span
+                        title="Rating"
+                        className={`text-xs font-semibold px-2.5 py-0.5 rounded ms-3 ${
+                            isNaN(Math.round(details?.vote_average * 10) / 10)
+                                ? "bg-gray-200 text-gray-800 dark:bg-gray-300 dark:text-gray-800"
+                                : getRatingColor(Math.round(details?.vote_average * 10) / 10)
+                        }`}
+                    >
+                        {isNaN(Math.round(details?.vote_average * 10) / 10)
+                            ? "N/A"
+                            : Math.round(details?.vote_average * 10) / 10}
+                    </span>
+                </div>
+                <div className="flex items-end justify-between">
+                    <div className="flex justify-between text-sm">
+                        <span className="mr-2 text-gray-900 dark:text-slate-300">
+                            ({releaseDate ? releaseDate.substring(0, 4) : "N/A"})
+                        </span>
+
+                        <span className="text-gray-900 dark:text-slate-300">
+                            {props.type === "movie" ? runtime + "m" : "Seasons: " + seasons}
+                        </span>
+                    </div>
+
+                    <Bookmark></Bookmark>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,24 +1,23 @@
 import MovieSlider from "./MovieSlider";
 import background from "../../assets/images/background.jpg";
-import TVSlider from "./TVSlider";
 
 export default function Home() {
-  return (
-    <div className="font-poppins">
-      <div className="relative mb-32">
-        <img
-          src={background}
-          className="max-h-96 w-full object-cover mix-blend-overlay"
-        ></img>
-        <h1 className="absolute bottom-8 left-8 font-bold text-4xl">
-          Discover and keep track of movies and TV shows in one place
-        </h1>
-      </div>
+    return (
+        <div className="font-poppins">
+            <div className="relative mb-32">
+                <img
+                    src={background}
+                    className="max-h-96 w-full object-cover mix-blend-overlay"
+                ></img>
+                <h1 className="absolute bottom-8 left-8 font-bold text-4xl">
+                    Discover and keep track of movies and TV shows in one place
+                </h1>
+            </div>
 
-      <MovieSlider name="Popular Movies" category="popular"></MovieSlider>
-      <MovieSlider name="Top Rated" category="top_rated"></MovieSlider>
-      <MovieSlider name="Now Playing" category="now_playing"></MovieSlider>
-      <TVSlider name="Popular TV Shows" category="popularTV"></TVSlider>
-    </div>
-  );
+            <MovieSlider name="Popular Movies" category="popular" type="movie"></MovieSlider>
+            <MovieSlider name="Top Rated" category="top_rated" type="movie"></MovieSlider>
+            <MovieSlider name="In theaters" category="now_playing" type="movie"></MovieSlider>
+            <MovieSlider name="Popular TV Shows" category="popular" type="tv"></MovieSlider>
+        </div>
+    );
 }
